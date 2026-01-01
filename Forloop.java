9049f1608828b8fb043ef15057a264a9e1f8977d
Rahul R <rrahulnair397@gmail.com>
2026-01-01 17:16:22 +0530

    Assignment

---
+++ Forloop.java
@@ -0,0 +1,21 @@
+package dAY26_mISSED;
+
+public class Forloop {
+
+	public static void main(String[] args) 
+	{
+		 for(int num=1;num<=10;num++)
+		 {
+			 System.out.print(num+" ");
+			 
+		}
+		 System.out.println();
+		 for(int num=10;num>=1;num--)
+		 {
+			 System.out.print(num+" ");
+		 }
+	}
+
+}
+//10
+//9
