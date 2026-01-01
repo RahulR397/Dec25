3c8be76eeb77216477245ff133aa8e3824206310
Rahul R <rrahulnair397@gmail.com>
2026-01-01 17:27:57 +0530

    Assignment

---
+++ RevrseOrderNumber.java
@@ -0,0 +1,18 @@
+package dAY26_mISSED;
+
+public class RevrseOrderNumber {
+//10 9 8 7 6 5 4 3 2 1 0..
+	public static void main(String[] args) 
+	{
+		int num=10;
+		while(num>=0)
+		{
+			System.out.println(num);
+			num--;
+		}
+		
+		
+
+	}
+
+}
