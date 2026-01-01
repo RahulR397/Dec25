0edb2d9060d88775af331e659c659e3cc5414de8
Rahul R <rrahulnair397@gmail.com>
2026-01-01 19:44:23 +0530

    natural number

---
+++ NaturalNumbers.java
@@ -0,0 +1,16 @@
+package dAY26_mISSED;
+
+public class NaturalNumbers {
+
+	public static void main(String[] args) 
+	{
+		int num=1;
+		while(num<=50)
+		{
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
